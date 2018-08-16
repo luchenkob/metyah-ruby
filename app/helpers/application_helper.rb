39d5bee2bf992c8a03391b2c0bde7fb96fd9c27f
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-16 19:39:19 +0000

    Update layout and views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_navigation?(controllers_name, actions_name = nil)
+    if controllers_name.include?(controller_name)
+      if actions_name.nil? || actions_name.include?(action_name)
+        'active'
+      end
+    end
+  end
 end
