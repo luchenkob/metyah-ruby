f0b2e8af07c6eca844b19c9f2b35e54d8405a843
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-16 18:52:18 +0000

    Scaffold events

---
+++ db/migrate/20180816184533_create_events.rb
@@ -0,0 +1,20 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.datetime :start_at
+      t.datetime :end_at
+      t.string :name
+      t.string :address
+      t.text :description
+      t.string :code
+      t.string :event_status
+      t.string :event_type
+      t.integer :display_profiles_after_minutes
+      t.integer :display_profile_for_minutes
+      t.integer :allow_messaging_after_minutes
+      t.integer :allow_messaging_for_minutes
+
+      t.timestamps
+    end
+  end
+end
