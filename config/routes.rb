63102a218709e5fed68cb701b9bcdf18e81de9be
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-19 05:42:39 +0000

    Setup partialization/separation of user/admin layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  namespace :user do
+    resources :events, only: [:show, :index]
+  end
+
   namespace :admin do
     resources :events, :places
   end
 
-  root "admin/events#index"
+  root "user/events#index"
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
