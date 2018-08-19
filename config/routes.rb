bc9b885c7e48e1f4a24f16360837e6fcdd3b95e7
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-19 06:08:55 +0000

    Add demo layout for user facing events index/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   namespace :user do
     resources :events, only: [:show, :index]
   end
+  get 'user', to: 'user/events#index'
 
   namespace :admin do
     resources :events, :places
   end
+  get 'admin', to: 'admin/events#index'
 
   root "user/events#index"
 end
