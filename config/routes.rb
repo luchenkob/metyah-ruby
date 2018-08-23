67ecad101906519251dbc4bb4a46ae18f7a98365
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-23 04:43:10 +0000

    Add messaging status message for inbox

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'user/events/current/:id/attendees' => 'user/events/current#attendees', as: :attendees_user_current_event
   get 'user/events/current/:id/favorites' => 'user/events/current#favorites', as: :favorites_user_current_event
   get 'user/events/current/:id/inbox' => 'user/events/current#inbox', as: :inbox_user_current_event
+  get 'user/events/current/:id' => 'user/events/current#inbox', as: :user_current_event
 
   get 'user', to: 'user/events#index'
   namespace :user do
