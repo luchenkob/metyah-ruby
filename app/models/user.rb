96a2945d31d95d3896469e41d599aee43396d1cb
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-24 05:24:22 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :first_name, :last_name, :birthday, :gender, presence: true
+
+  validates :bio, length: { maximum: 150 }
+
+  GENDER_MALE = "Male"
+  GENDER_FEMALE = "Female"
+  GENDERS = [GENDER_MALE, GENDER_FEMALE]
+  validates :gender, :inclusion => { :in => User::GENDERS }
+
 end
