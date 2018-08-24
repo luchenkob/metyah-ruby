77f26949494aa2ee385d568731b6ee4549939558
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-24 05:02:00 +0000

    Add profile fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_042255) do
+ActiveRecord::Schema.define(version: 2018_08_24_050010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 2018_08_24_042255) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.string "gender"
+    t.string "career"
+    t.string "school"
+    t.string "location"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
