20a4220007a2a381c2012ac3b0fc9cd35110608c
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-24 04:58:02 +0000

    Add devise views for users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,10 @@
 class UserController < ApplicationController
   # All user controllers should inherit from this controller
-  before_action :authenticate_user!
 
   layout "user"
+
+   # Refactor to handle admin/host/user authentication
+   # => https://github.com/plataformatec/devise/wiki/How-to-Setup-Multiple-Devise-User-Models
+   #    => May need to add sessions/registrations controllers
+  before_action :authenticate_user!
 end
