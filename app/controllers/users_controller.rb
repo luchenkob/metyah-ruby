5ca3a8fe3a2ecc4adbb2c1775a3ad105a7ee2c65
Bogdan Luchenko <luchenkob@gmail.com>
2018-08-30 03:03:31 +0000

    Setup profile photo crop/upload with UploadCare

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    redirect_to user_profile_path
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_profile_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render 'user/profile/profile' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      user_params_hash = params.require(:user).permit(
+        :email, :first_name, :last_name, :birthdate, :gender,
+        :career, :school, :location, :bio,
+        :photo
+      )
+
+      user_params_hash[:birthdate] = DateTime.strptime(user_params_hash["birthdate"], "%m/%d/%Y %H:%M %p")
+
+      user_params_hash
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_uploadcare_file :photo
+
   has_many :event_users, dependent: :destroy
   has_many :events, through: :event_users
 
@@ -11,10 +13,16 @@ class User < ApplicationRecord
 
   validates :bio, length: { maximum: 150 }
 
+  before_validation :standardize
+
   GENDER_MALE = "Male"
   GENDER_FEMALE = "Female"
   GENDERS = [GENDER_MALE, GENDER_FEMALE]
   validates :gender, :inclusion => { :in => User::GENDERS }
+
+  def standardize
+    #self.birthdate = DateTime.strptime(birthdate, "%m/%d/%Y %H:%M %p")
+  end
 
   def age
     # Source: https://medium.com/@craigsheen/calculating-age-in-rails-9bb661f11303
