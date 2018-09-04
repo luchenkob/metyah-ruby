883a815285b44e6a4f3e6b3e60d4a9ad1d5609ed
Bogdan Luchenko <luchenkob@gmail.com>
2018-09-04 03:30:30 +0000

    Add private message validations

--- app/models/user/private_message.rb
+++ app/models/user/private_message.rb
@@ -1,4 +1,7 @@
 class User::PrivateMessage < ApplicationRecord
   belongs_to :sender, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
+
+  validates :content, :recipient_id, :sender_id, presence: true
+  validates :content, length: { maximum: 250 }
 end
