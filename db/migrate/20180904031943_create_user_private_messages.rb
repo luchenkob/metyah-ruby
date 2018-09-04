ef616ce336eaa01903806ab48c969050e1ca2841
Bogdan Luchenko <luchenkob@gmail.com>
2018-09-04 03:25:50 +0000

    Fix migration and run migrations

--- db/migrate/20180904031943_create_user_private_messages.rb
+++ db/migrate/20180904031943_create_user_private_messages.rb
@@ -2,10 +2,13 @@ class CreateUserPrivateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :user_private_messages do |t|
       t.text :content
-      t.references :sender, foreign_key: true
-      t.references :recipient, foreign_key: true
+      t.references :sender
+      t.references :recipient
 
       t.timestamps
     end
+
+    add_foreign_key :user_private_messages, :users, column: :sender_id, primary_key: :id
+    add_foreign_key :user_private_messages, :users, column: :recipient_id, primary_key: :id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_045302) do
+ActiveRecord::Schema.define(version: 2018_09_04_031943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,60 @@ ActiveRecord::Schema.define(version: 2018_08_30_045302) do
     t.index ["place_id"], name: "index_events_on_place_id"
   end
 
+  create_table "mailboxer_conversation_opt_outs", id: :serial, force: :cascade do |t|
+    t.string "unsubscriber_type"
+    t.integer "unsubscriber_id"
+    t.integer "conversation_id"
+    t.index ["conversation_id"], name: "index_mailboxer_conversation_opt_outs_on_conversation_id"
+    t.index ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type"
+  end
+
+  create_table "mailboxer_conversations", id: :serial, force: :cascade do |t|
+    t.string "subject", default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mailboxer_notifications", id: :serial, force: :cascade do |t|
+    t.string "type"
+    t.text "body"
+    t.string "subject", default: ""
+    t.string "sender_type"
+    t.integer "sender_id"
+    t.integer "conversation_id"
+    t.boolean "draft", default: false
+    t.string "notification_code"
+    t.string "notified_object_type"
+    t.integer "notified_object_id"
+    t.string "attachment"
+    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false
+    t.boolean "global", default: false
+    t.datetime "expires"
+    t.index ["conversation_id"], name: "index_mailboxer_notifications_on_conversation_id"
+    t.index ["notified_object_id", "notified_object_type"], name: "index_mailboxer_notifications_on_notified_object_id_and_type"
+    t.index ["notified_object_type", "notified_object_id"], name: "mailboxer_notifications_notified_object"
+    t.index ["sender_id", "sender_type"], name: "index_mailboxer_notifications_on_sender_id_and_sender_type"
+    t.index ["type"], name: "index_mailboxer_notifications_on_type"
+  end
+
+  create_table "mailboxer_receipts", id: :serial, force: :cascade do |t|
+    t.string "receiver_type"
+    t.integer "receiver_id"
+    t.integer "notification_id", null: false
+    t.boolean "is_read", default: false
+    t.boolean "trashed", default: false
+    t.boolean "deleted", default: false
+    t.string "mailbox_type", limit: 25
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_delivered", default: false
+    t.string "delivery_method"
+    t.string "message_id"
+    t.index ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
+    t.index ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
+  end
+
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.string "timezone"
@@ -62,6 +116,16 @@ ActiveRecord::Schema.define(version: 2018_08_30_045302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profile_photos_on_user_id"
+  end
+
+  create_table "user_private_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "sender_id"
+    t.bigint "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_user_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_user_private_messages_on_sender_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -88,5 +152,10 @@ ActiveRecord::Schema.define(version: 2018_08_30_045302) do
   add_foreign_key "event_users", "events"
   add_foreign_key "event_users", "users"
   add_foreign_key "events", "places"
+  add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", column: "conversation_id", name: "mb_opt_outs_on_conversations_id"
+  add_foreign_key "mailboxer_notifications", "mailboxer_conversations", column: "conversation_id", name: "notifications_on_conversation_id"
+  add_foreign_key "mailboxer_receipts", "mailboxer_notifications", column: "notification_id", name: "receipts_on_notification_id"
   add_foreign_key "profile_photos", "users"
+  add_foreign_key "user_private_messages", "users", column: "recipient_id"
+  add_foreign_key "user_private_messages", "users", column: "sender_id"
 end
