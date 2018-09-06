74e6c2323b78e8ba8dd2c72e5f1816f90622f4b1
Bogdan Luchenko <luchenkob@gmail.com>
2018-09-06 08:14:11 +0000

    Add voting forms for favorite and block

--- app/controllers/user/voting_controller.rb
+++ app/controllers/user/voting_controller.rb
@@ -1,4 +1,19 @@
 class User::VotingController < ApplicationController
   def vote
+    @voter = User.find(params[:voter_id])
+    @votable = User.find(params[:votable_id])
+    @action = params[:action].to_s.downcase.to_sym
+    @action_scope = params[:action_scope].to_s.downcase.to_sym
+
+    case action
+    when :upvote
+      @voter.upvotes(@votable)
+    when :downvote
+      @voter.downvotes(@votable)
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
