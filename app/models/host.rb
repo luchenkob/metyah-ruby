618347b84f34db34183799f2c3a64874def4a240
Bogdan Luchenko <luchenkob@gmail.com>
2018-09-08 01:03:39 +0000

    Don't allow host sign up

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,6 +1,7 @@
 class Host < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
+         # :registerable, # Registration not allowed
          :recoverable, :rememberable, :validatable
 end
