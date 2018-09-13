bb5d4ab63f8210220184e500ccc3c04b465e395c
Bogdan Luchenko <luchenkob@gmail.com>
2018-09-13 01:14:22 +0000

    Use /info instead of /current

--- config/routes.rb
+++ config/routes.rb
@@ -22,11 +22,11 @@ Rails.application.routes.draw do
   resources :users, :controller=>:users, except: [:new, :create, :index]
 
   # These don't seem to like namespacing much
-  get 'user/events/current/:id/attendees' => 'user/events/current#attendees', as: :attendees_user_current_event
-  get 'user/events/current/:id/favorites' => 'user/events/current#favorites', as: :favorites_user_current_event
-  get 'user/events/current/:id/inbox' => 'user/events/current#inbox', as: :inbox_user_current_event
-  get 'user/events/current/:id' => 'user/events/current#attendees', as: :user_current_event
-  post 'user/events/current/:id/message' => 'user/events/current#message_modal', as: :user_current_message_modal
+  get 'user/events/info/:id/attendees' => 'user/events/current#attendees', as: :attendees_user_current_event
+  get 'user/events/info/:id/favorites' => 'user/events/current#favorites', as: :favorites_user_current_event
+  get 'user/events/info/:id/inbox' => 'user/events/current#inbox', as: :inbox_user_current_event
+  get 'user/events/info/:id' => 'user/events/current#attendees', as: :user_current_event
+  post 'user/events/info/:id/message' => 'user/events/current#message_modal', as: :user_current_message_modal
 
 
   get 'user/events' => 'user/events#index'
